1547ca7aefb23dd50f5dd3f2ab48c8fb13e1a1e3
Ken Todoroki <ken.todoroki@majimena.org>
2015-08-30 15:32:34 +0900

    add profile page detail.

--- server/api/seeker/seeker.controller.js
+++ server/api/seeker/seeker.controller.js
@@ -4,10 +4,10 @@ var _ = require('lodash');
 var Seeker = require('./seeker.model');
 
 var data = [
-  {'_id' : '0',  'mailaddress' : '',  'name' : '松尾利彦', 'motivated':'不明', 'area':[], 'social':[],  'sex' : '',  'birth' : '',  'marry' : '',  'address' : '茨城県つくばみらい市',  'work' : 'リクルートエージェント',  'university' : '福岡大学',  'friends' : '',  'Posting' : ''  },
-  {'_id' : '1',  'mailaddress' : 'yamada_taro@gmail.com',  'name' : '山田太郎', 'motivated':'悪い', 'area':['HTML'], 'social':[],  'sex' : '男',  'birth' : '29556',  'marry' : '既婚',  'address' : '東京都練馬',  'work' : '東京技術社',  'university' : '',  'friends' : '',  'Posting' : ''  },
-  {'_id' : '2',  'mailaddress' : 'suzuki_jiro@gmail.com',  'name' : '鈴木二郎', 'motivated':'かなり良い', 'area':['Architecture','Management','Java','HTML5','Oracle'], 'social':['Facebook','Twitter','Linked In','GitHub'],  'sex' : '男',  'birth' : '32172',  'marry' : '未婚',  'address' : '千葉県浦安市',  'work' : '鬼山株式会社',  'university' : '千葉大学',  'friends' : '',  'Posting' : ''  },
-  {'_id' : '3',  'mailaddress' : 'yamada_atsuko@yahoo.co.jp',  'name' : '山田敦子', 'motivated':'良い', 'area':['Document'], 'social':['Facebook','Twitter'],  'sex' : '女',  'birth' : '29292',  'marry' : '未婚',  'address' : '北海道苫小牧',  'work' : '',  'university' : '北海道大学',  'friends' : '',  'Posting' : ''}
+  {'_id' : '0', image:'https://cdn0.iconfinder.com/data/icons/user-pictures/100/male-128.png',  'mailaddress' : '',  'name' : '松尾利彦', 'motivated':'不明', 'area':[], 'social':[],  'sex' : '',  'birth' : '',  'marry' : '',  'address' : '茨城県つくばみらい市',  'work' : 'リクルートエージェント',  'university' : '福岡大学',  'friends' : '',  'Posting' : ''  },
+  {'_id' : '1', image:'https://cdn1.iconfinder.com/data/icons/IconsLandVistaPeopleIconsDemo/128/Barbarian_Male.png',  'mailaddress' : 'yamada_taro@gmail.com',  'name' : '山田太郎', 'motivated':'悪い', 'area':['HTML'], 'social':[],  'sex' : '男',  'birth' : '29556',  'marry' : '既婚',  'address' : '東京都練馬',  'work' : '東京技術社',  'university' : '',  'friends' : '',  'Posting' : ''  },
+  {'_id' : '2', image:'https://cdn0.iconfinder.com/data/icons/user-pictures/100/malecostume-128.png',  'mailaddress' : 'suzuki_jiro@gmail.com',  'name' : '鈴木二郎', 'motivated':'かなり良い', 'area':['Architecture','Management','Java','HTML5','Oracle'], 'social':['Facebook','Twitter','Linked In','GitHub'],  'sex' : '男',  'birth' : '32172',  'marry' : '未婚',  'address' : '千葉県浦安市',  'work' : '鬼山株式会社',  'university' : '千葉大学',  'friends' : '',  'Posting' : ''  },
+  {'_id' : '3', image:'https://cdn0.iconfinder.com/data/icons/user-pictures/100/female-128.png', 'mailaddress' : 'yamada_atsuko@yahoo.co.jp',  'name' : '山田敦子', 'motivated':'良い', 'area':['Document'], 'social':['Facebook','Twitter'],  'sex' : '女',  'birth' : '29292',  'marry' : '未婚',  'address' : '北海道苫小牧',  'work' : '',  'university' : '北海道大学',  'friends' : '',  'Posting' : ''}
 ];
 
 // Get list of seekers
